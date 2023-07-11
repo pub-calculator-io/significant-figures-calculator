d4bd8bafe7b423a998efe051d31bd2019897565e
Arseniy Olevskiy <77734966+arolus@users.noreply.github.com>
2023-07-11 09:05:00 +0500

    regular commit

---
+++ assets/js/calculator.js
@@ -0,0 +1,47 @@
+function calculate(){
+  // 1. init & validate
+  const number = input.get('number').scientific().raw();
+  const sigFig = input.get('significant_figures').natural().lt(16).val();
+  if(!input.valid()) return;
+
+  // 2. calculate
+  const [,sign,coeff,base] = number.replaceAll(' ','').match(/^(-)?([\d\.]+)(.+)?$/);
+  
+  // get significant figures
+  // - any non-zero digit
+  // - and zeros between non-zero digits
+  // - and trailing zeros when there is a decimal point
+  // - no leading zeros
+  let result = coeff.replace(/^-?(([1-9]+(0+[1-9]+)*)\d*|0?\.0*(\d*))$/,'$2$4').replace('.','');
+  
+  // round
+  result = math.format(math.round('0.'+result,sigFig),{notation:'fixed'}).slice(2);
+  // append zeros
+  if(result.length < sigFig){
+    result+= '0'.repeat(sigFig-result.length);
+  }
+
+  // restore number
+  if(!['0','.'].includes(coeff[0])){
+    let dotIndex = coeff.indexOf('.');
+    if(dotIndex == -1) {
+      dotIndex = coeff.length;
+    }
+    if(result.length <= dotIndex) {
+      result+= '0'.repeat(dotIndex - result.length);
+    } else {
+      result = result.slice(0,dotIndex) + '.' + result.slice(dotIndex);
+    }
+  } 
+  else {
+    let zeros = (coeff[0] == '.' ? 1 : 0);
+    zeros+= coeff.replace('.','').match(/^0*/)[0].length - 1;
+    result = '0.' + '0'.repeat(zeros) + result;
+  }
+  result = (sign || '') + result + (base || '');
+  const formattedResult = math.format(math.number(result));
+
+  // 3. output
+  _('result').innerHTML = result + (formattedResult != result ? ' or ' + formattedResult : '');
+
+}
---
+++ readme.txt
@@ -0,0 +1,30 @@
+=== Significant Figures Calculator by Calculator.iO ===
+Contributors: calculatorio
+Tags: 
+Requires at least: 5.0
+Tested up to: 6.2.2
+Stable tag: 1.0.0
+License: GPLv2 or later
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+
+The significant figure rounder rounds numbers to the required quantity of significant figures. It works with a standard number format, e-notation, and scientific notation.
+
+== Description ==
+
+The significant figure rounder rounds numbers to the required quantity of significant figures. It works with a standard number format, e-notation, and scientific notation.
+
+== Installation ==
+
+1. Upload the /ci_significant_figures_calculator/ folder to the /wp-content/plugins/ directory.
+2. Activate the [Significant Figures Calculator](https://www.calculator.io/significant-figures-calculator/ "Significant Figures Calculator Homepage") plugin through the "Plugins" menu in WordPress.
+3. Configure your settings via Appearance > Widgets > Significant Figures Calculator.
+4. And you're good to go!
+
+== Screenshots ==
+1. The Significant Figures Calculator Input Form.
+2. The Significant Figures Calculator Calculation Results.
+
+== Changelog ==
+
+= 1.0.0 =
+* Initial release
